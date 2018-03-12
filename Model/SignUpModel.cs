01753766785ea9ae45ed6298c34cc576b6c73f5f
Dmitry <dmitry.drobotov123@gmail.com>
2018-03-12 21:12:31 +0300

    Add view without bindings and commands.

---
+++ Model/SignUpModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Qoden.Binding;
+
+namespace SignUp
+{
+    public class SignUpModel : DataContext
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+        private Command SignUpCommand;
+
+        public SignUpModel()
+        {
+            SignUpCommand = new Command()
+            {
+                Action = PerformSignUp,
+                CanExecute = FieldsAreValid
+            };
+        }
+
+        private bool FieldsAreValid(object arg) =>
+            Login.Length >= 3 && Password.Equals(ConfirmPassword);
+
+        private void PerformSignUp(object obj)
+        {
+            //TODO: show alert or something
+        }
+    }
+}
